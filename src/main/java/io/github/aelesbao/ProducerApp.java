28c23b131927f34369b2e2daaf7fda6dda50dce8
Augusto Elesbão <aelesbao@gmail.com>
2017-01-01 23:07:07 +0100

    Add more settings to producer initialization

--- src/main/java/io/github/aelesbao/ProducerApp.java
+++ src/main/java/io/github/aelesbao/ProducerApp.java
@@ -16,9 +16,23 @@ public class ProducerApp {
 
         // Create the Properties class to instantiate the Producer with the desired settings:
         Properties props = new Properties();
-        props.put("bootstrap.servers", "0.0.0.0:32806, 0.0.0.0:32810, 0.0.0.0:32808");
+        props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "1");
+        props.put("buffer.memory", 33554432);
+        props.put("compression.type", "none");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("client.id", "");
+        props.put("linger.ms", 0);
+        props.put("max.block.ms", 60000);
+        props.put("max.request.size", 1048576);
+        props.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
+        props.put("request.timeout.ms", 30000);
+        props.put("timeout.ms", 30000);
+        props.put("max.in.flight.requests.per.connection", 5);
+        props.put("retry.backoff.ms", 5);
 
         String topic = "my-topic";
         int numberOfRecords = 100; // number of records to send
